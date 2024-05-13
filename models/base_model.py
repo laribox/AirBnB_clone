def60e2519b3d16123610b2565dc04354e0666b6
mrclench <charlesnnnortey@gmail.com>
2024-05-13 10:54:26 +0000

    fix styling issues

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,8 @@ class BaseModel:
         Initializes a new instance of BaseModel with the given name.
         """
         self.id = str(uuid.uuid4())  # Generate a unique ID for each instance
-        self.created_at = datetime.now()  # Assign current datetime when instance is created
-        self.updated_at = datetime.now()  # Assign current datetime when instance is created
+        self.created_at = datetime.now()  # Assign current datetime
+        self.updated_at = datetime.now()  # Assign current datetime
 
     def __str__(self):
         """Returns a string representation of an object"""
@@ -27,9 +27,9 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns data in the dictionary (currently time created and time updated)"""
+        """Returns data in the dictionary"""
         obj_dict = self.__dict__.copy()
-        """Using obj_dict to arrange the tuple and process the data that is in JSON"""
+        """Arrange the tuple and process the data that is in JSON"""
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
