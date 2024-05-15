8fa713b91f7127b1bb9892b4ec36e9760a8e4fe9
mrclench <charlesnnnortey@gmail.com>
2024-05-15 08:33:28 +0000

    comments added

--- models/user.py
+++ models/user.py
@@ -3,6 +3,9 @@
 from basemodel import BaseModel
 
 class User(BaseModel):
+    """
+    Creating class user that inherits from BaseModel class
+    """
     email = ''
     password = ''
     first_name = ''
