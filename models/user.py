18d712b350946fdc25cde9580391c7f609f28c2e
mrclench <charlesnnnortey@gmail.com>
2024-05-15 08:30:46 +0000

    user class modified

--- models/user.py
+++ models/user.py
@@ -3,9 +3,8 @@
 from basemodel import BaseModel
 
 class User(BaseModel):
-    def __init__(self, email, password, first_name, last_name)
-        self.email = ''
-        self.password = ''
-        self.first_name = ''
-        self.last_name = ''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
 
