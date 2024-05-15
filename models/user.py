008f900378f808179500b737dae1dabad8f6c206
mrclench <charlesnnnortey@gmail.com>
2024-05-15 08:23:53 +0000

    creating user class

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from basemodel import BaseModel
+
+class User(BaseModel):
+    def __init__(self, email, password, first_name, last_name)
+        self.email = ''
+        self.password = ''
+        self.first_name = ''
+        self.last_name = ''
+
