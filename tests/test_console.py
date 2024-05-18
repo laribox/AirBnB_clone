d9040a2e23eb8e7bcff85698b079df401019e0af
laribox <laribox2014@gmail.com>
2024-05-18 08:21:29 -0700

    started working on test_console and moved the engine dir to the right place

---
+++ tests/test_console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import unittest
+from console import HBNBCommand
+
+class TestHBNBCommand(unittest.TestCase):
+
+    def test_costum_prompt(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+        
+        
