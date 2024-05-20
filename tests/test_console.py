86b7fbfac37295fe844eca26566bcf6e573d36d9
laribox <laribox2014@gmail.com>
2024-05-20 04:28:04 -0700

    added tests to test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,6 +20,18 @@ class TestHBNBCommand(unittest.TestCase):
     def test_EOF_exits(self):
         self.assertTrue(HBNBCommand().onecmd("EOF"))
 
+    def test_create_missing_class(self):
+        errorString = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("create")
+            self.assertEqual(errorString, output.getvalue().strip())
+
+    def test_create_invalid_class(self):
+        errorString = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("create MyModel")
+            self.assertEqual(errorString, output.getvalue().strip())
+
     def test_create_object(self):
         with patch("sys.stdout", new=StringIO()) as output:
             HBNBCommand().onecmd("create BaseModel")
