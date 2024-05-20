b7272fe25895fae8335932637c87e8339dc7c2e6
laribox <laribox2014@gmail.com>
2024-05-20 03:56:28 -0700

    added tests to test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,6 +9,11 @@ class TestHBNBCommand(unittest.TestCase):
     def test_costum_prompt(self):
         self.assertEqual("(hbnb) ", HBNBCommand.prompt)
 
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
+
     def test_quit_exits(self):
         self.assertTrue(HBNBCommand().onecmd("quit"))
 
