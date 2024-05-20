5fdef05d277dbe2f97734053d0f2e54ede27c79c
laribox <laribox2014@gmail.com>
2024-05-20 15:12:59 -0700

    fixed city class

--- models/city.py
+++ models/city.py
@@ -8,21 +8,3 @@ class City(BaseModel):
     """Class for managing city"""
     state_id = ""
     name = ""
-
-    def name(self):
-        return self._name
-
-    def name(self, value):
-        if value is not None:
-            self._name = value
-        else:
-            raise ValueError("name cannot be None")
-
-    def state_id(self):
-        return self._state_id
-
-    def state_id(self, value):
-        if value is not None:
-            self._state_id = value
-        else:
-            raise ValueError("state_id cannot be None")
